41898f2c12ae93b2ab443c259b63896768881623
Israel Cunha <israel.terto27@gmail.com>
2018-09-12 13:58:39 -0300

    Comeco

--- project/src/main/java/EmailAccount.java
+++ project/src/main/java/EmailAccount.java
@@ -3,14 +3,21 @@ import java.time.LocalDate;
 public class EmailAccount {
     private String user;
     private String domain;
-    private String password;
-    private LocalDate lastPasswordUpdate;
+    private String password = "16/06/2018";
+    private LocalDate lastPasswordUpdate = LocalDate.now();
 
     public void setLastPasswordUpdate(LocalDate lastPasswordUpdate) {
         this.lastPasswordUpdate = lastPasswordUpdate;
     }
 
-    public boolean verifyPasswordExpiration(String password){
-        throw new UnsupportedOperationException();
+    public boolean verifyPasswordExpiration(String password) {
+
+        if ()
+
+            return true;
+             else
+            return false;
+
+        }
     }
-}
+
--- project/src/test/java/EmailAccountTest.java
+++ project/src/test/java/EmailAccountTest.java
@@ -1,19 +1,31 @@
 
+import com.sun.org.apache.xpath.internal.operations.Equals;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EmailAccountTest {
 
     EmailBuilder emailBuilder;
+    EmailAccount emailAccount;
     LocalDate now = LocalDate.now();
 
     @BeforeEach
     public void setUpTest() {
 
         this.emailBuilder = new EmailBuilder();
+    }
 
-
+    @Test
+    public void verifyPasswordExpiration(){
+       emailBuilder.setCreationDate(Instant.now());
+       emailAccount.setLastPasswordUpdate(LocalDate.now());
+       assertTrue(() -> true);
     }
 }
