c5161c8131d92ec85b2c83c9d74c0cc86c6ab99f
israelcunha <israel.terto27@gmail.com>
2018-09-18 23:57:03 -0300

    adicionar Sonar 4

--- project/src/test/java/EmailClientTest.java
+++ project/src/test/java/EmailClientTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
